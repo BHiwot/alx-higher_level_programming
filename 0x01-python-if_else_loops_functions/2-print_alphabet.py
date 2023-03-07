936749c84e503793d1cf25d6e3920b34052f91fa
Hiwot Berhanu <hmmmfb@gmail.com>
2023-03-07 05:28:28 -0800

    print alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for letter_code in range(ord('a'), ord('z')+1):
-    print("{:c}".format(letter_code), end="")
+for b in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(b), end="")
